84963d78dc0781a22ad3d2886def94199e5c678a
Soumya Sundarrajan <soumya.rajan86@gmail.com>
2015-08-25 16:47:23 +0100

    Bulk upload
    Program for uploading bulk data into GAE cloud storage.

---
+++ main.py
@@ -0,0 +1,87 @@
+#Open dataset names hardcoded in the program are retrived from following sources-
+#Supermarkets - http://geolytix.co.uk/blog/tag/supermarkets/
+#Train stations  - http://data.gov.uk/dataset/naptan
+#Schools  - http://www.education.gov.uk/edubase/home.xhtml
+#GP Surgeries - http://data.gov.uk/dataset/england-nhs-connecting-for-health-organisation-data-service-data-files-of-general-medical-practices
+#Postcodes and area codes - http://www.freemaptools.com/download-uk-postcode-lat-lng.htm
+#Postcodes - http://www.doogal.co.uk/UKPostcodes.php?
+
+import logging
+import os
+import csv
+import cloudstorage as gcs
+import webapp2
+
+from google.appengine.api import app_identity
+
+# Retry can help overcome transient urlfetch or GCS issues, such as timeouts.
+my_default_retry_params = gcs.RetryParams(initial_delay=0.2,
+                                          max_delay=5.0,
+                                          backoff_factor=2,
+                                          max_retry_period=15)
+gcs.set_default_retry_params(my_default_retry_params)
+
+
+class MainPage(webapp2.RequestHandler):
+	"""Main page for GCS demo application."""
+
+	def get(self):
+		bucket_name = os.environ.get('local-amenities.appspot.com', app_identity.get_default_gcs_bucket_name())
+		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.write('Demo GCS Application running from Version: '
+                        + os.environ['CURRENT_VERSION_ID'] + '\n')
+		self.response.write('Using bucket name: ' + bucket_name + '\n\n')
+		bucket = '/' + bucket_name
+		filename = bucket + '/BT_postcodes_1.csv'
+		filename2 = bucket + '/BT_postcodes_2.csv'
+		filename3 = bucket + '/BT_postcodes_3.csv'
+		filename4 = bucket + '/EH_postcodes_1.csv'
+		filename5 = bucket + '/EH_postcodes_2.csv'
+		filename6 = bucket + '/G_postcodes_1.csv'
+		filename7 = bucket + '/G_postcodes_2.csv'
+		self.tmp_filenames_to_clean_up = []
+		
+		try:
+			self.create_file(filename, 'BT_postcodes_1.csv')
+			self.create_file(filename2, 'BT_postcodes_2.csv')
+			self.create_file(filename3, 'BT_postcodes_3.csv')
+			self.create_file(filename4, 'EH_postcodes_1.csv')
+			self.create_file(filename5, 'EH_postcodes_2.csv')
+			self.create_file(filename6, 'G_postcodes_1.csv')
+			self.create_file(filename7, 'G_postcodes_2.csv')
+			self.read_file(filename7)
+			self.response.write('\n\n')
+
+		except Exception, e:  # pylint: disable=broad-except
+			logging.exception(e)
+			self.response.write('\n\nThere was an error running the demo! '
+								'Please check the logs for more details.\n')
+ 
+# When writing a file to Cloud Storage, you should not call finally:close() as
+# this can result in finalizing object uploads even if there was an exception
+# during a write.
+	def create_file(self, filename, local_file):
+		self.response.write('Creating file %s\n' % filename)
+		write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+		gcs_file = gcs.open(filename,
+							'w',
+							content_type='text/csv',
+							retry_params=write_retry_params)
+		f=open(local_file)
+		reader=csv.reader(f)
+		writer = csv.writer(gcs_file)
+		for row in reader:        
+			writer.writerow(row)
+		gcs_file.close()
+		self.tmp_filenames_to_clean_up.append(filename)
+
+	def read_file(self, filename):
+		self.response.write('Abbreviated file content (first line and last 1K):\n')
+		gcs_file = gcs.open(filename)
+		self.response.write(gcs_file.readline())
+		gcs_file.seek(-1024, os.SEEK_END)
+		self.response.write(gcs_file.read())
+		gcs_file.close()
+
+app = webapp2.WSGIApplication([('/', MainPage)],
+                              debug=True)
